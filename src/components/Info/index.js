4a82e5c2c3461801771c210f7d792ecd19146936
Zindiks <103096207+Zindiks@users.noreply.github.com>
2022-09-22 05:22:25 +0300

    little changes

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,17 +1,17 @@
-import Style from "../Info/info.module.scss"
+import Style from '../Info/info.module.scss';
+import { Link } from 'react-router-dom';
 
-
-export default function Order({title,description,image,action}) {
+export default function Order({ title, description, image, action }) {
   return (
     <div className={Style.cartEmpty}>
       <img src={image} alt="complete-order" />
       <h2>{title}</h2>
-      <p className="opacity-6">
-        {description}
-      </p>
-      <button className={Style.greenButton} onClick={action}>
-        <img src="/img/arrow.svg" alt="arrow" /> Вернуться назад
-      </button>
+      <p className="opacity-6">{description}</p>
+      <Link to="/">
+        <button className={Style.greenButton} onClick={action}>
+          <img src="/img/arrow.svg" alt="arrow" /> Вернуться назад
+        </button>
+      </Link>
     </div>
   );
 }
